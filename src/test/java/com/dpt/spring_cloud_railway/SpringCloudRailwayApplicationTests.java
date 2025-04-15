b2166e32cfd9d87209a478211aab199e3789f84b
Douglas <douglaspt@gmail.com>
2025-04-15 19:46:13 -0300

    Initial commit

---
+++ src/test/java/com/dpt/spring_cloud_railway/SpringCloudRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpt.spring_cloud_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
